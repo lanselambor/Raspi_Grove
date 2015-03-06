2d451e63c2bf7a47431c84d01ccf1d4a9d0ef095
lanselambor <757016515@qq.com>
2015-03-06 05:52:45 +0000

    update script build_tools.sh to install the neccessary tools

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Raspi_Grove_Adaption
-Connect Grove modules to Raspi IO straightly(using RPi.GPIO and WiringPi libraries)
+Description:Connect Grove modules to Raspi IO straightly(using RPi.GPIO and WiringPi libraries)
+
+step 1:Install neccessary tools by typing the cmd_line 
+
+  $sudo ./build_tools.sh
+
+step2:Clone the WiringPi2-Python repository 
+
+  $git clone https://github.com/WiringPi/WiringPi2-Python
+
+
---
+++ build_tools.sh
@@ -0,0 +1,24 @@
+
+apt-get update
+sudo apt-get upgrade
+
+install tools
+apt-get install python-pip
+apt-get install python-setuptools
+apt-get install python-dev
+easy_install -U distribute
+pip install RPi.GPIO
+apt-get install i2c-tools python-smbus
+
+
+#Install i2c module
+echo "adding \"i2c-bcm2708 i2c-dev\" to /etc/modules\n"
+chmod 666 /etc/modules
+
+echo "i2c-bcm2708\ni2c-dev" >> /etc/modules
+chmod 644 /etc/modules
+cat /etc/modules
+
+
+#reboot the systerm and check if i2c module install successfully
+#i2cdetect -y -a 1
